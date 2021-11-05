bde67189c311b6263011652645634cede93d42e6
arrass@microsoft.com <arash.rassoulpour@microsoft.com>
2021-11-05 09:32:00 +0100

    created

---
+++ WebSocketClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Websocket.Client;
+
+namespace WebSocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var exitEvent = new ManualResetEvent(false);
+            var url = new Uri("wss://apimarra.azure-api.net/deltaws");
+            //var url = new Uri("wss://deltatest1.azurewebsites.net/ws");
+            using (var client = new WebsocketClient(url))
+            {
+                client.ReconnectTimeout = TimeSpan.FromSeconds(30);
+                client.ReconnectionHappened.Subscribe(info =>
+                    Console.WriteLine($"Reconnection happened, type: {info.Type}"));
+
+                client.MessageReceived.Subscribe(msg => Console.WriteLine($"Message received: {msg}"));
+                client.Start();
+
+                Task.Run(() => client.Send("{ message }"));
+
+                exitEvent.WaitOne();
+            }
+        }
+    }
+}
